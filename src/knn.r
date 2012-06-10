00ecda74e25a7b37ac8aeef6e333817babc186ca
Piotr Tomasz Monarski <piotr@monarski.pl>
2012-06-10 04:16:51 +0200

    knn w modelu kompresuje wektroy by nie operować domyślnych gigantach

--- src/knn.r
+++ src/knn.r
@@ -2,19 +2,40 @@
 # Klasyfikator K-NN
 #
 
-# FUNC train.knn(data, fact)
+# FUNC metric.euclid(v1,v2) 
+# 
+# Funkcja liczy odległość pomiędzy wektorami zgodnie z metryką euklidesową.
 #
-# data - ramka danych zawierajaca liczby
-# fact - faktor zawierajacy poprawna klasyfikacje
-# tekstow nalezacych do data
+# v1 - pierwszy wektor
+# v2 - drugi wektor
 #
-# RETURN obiekt klasy mcTbnb do uzycia w funkcji predict
+# RETURN odległość pomiędzy wektorami 
 #
-metric.euc <- function(v1,v2) {
-  v3 <- v2 - v1
-  v3 <- v3^2
-  dist <- sum(v3)
-  return (sqrt(dist))
+metric.euclid <- function(v1,v2) {
+  return (sqrt (sum ((v2 - v1)^2)))
+}
+
+# FUNC data.compact(data) 
+# 
+# Funkcja kompresuje dane (usuwa kolumny z wartością zero) na potrzeby
+# algorytmu knn.
+#
+# data - tablica danych 
+#
+# RETURN lista ramek reprezentująca indeksy słów i ich liczność.
+#
+data.compact <- function (data){
+  vectors <- list()
+
+  for (i in 1:nrow(data)){
+    zeroes <- data[i,] == 0
+    id <- (1:ncol(data))[!zeroes]
+    count <- data[i,][!zeroes]
+    vec <- data.frame (id=id, count=count)
+    vectors <- append (vectors, list(vec))
+  }
+
+  return (vectors)
 }
 
 # FUNC train.knn(data, fact)
@@ -23,23 +44,24 @@ metric.euc <- function(v1,v2) {
 # fact - faktor zawierajacy poprawna klasyfikacje
 # tekstow nalezacych do data
 #
-# RETURN obiekt klasy mcTbnb do uzycia w funkcji predict
+# RETURN obiekt klasy ptmKnn do uzycia w funkcji predict
 #
 train.knn <- function(data, fact, k=3, metric=metric.cos) {
-  m <- list(data=data, fact=fact, k=k, metric=metric)
-  class(m) <- 'ptmKnn'
-  return (m)
+
+  vectors <- data.compact(data)
+  model <- list(vectors=vectors, fact=fact, k=k, metric=metric)
+
+  class(model) <- 'ptmKnn'
+  return (model)
 }
 
 # FUNC predict.ptmKnn(model, data)
 #
-# Rozszerzenie funkcji generycznej predict.
-# Obiekt model musi byc lista zawierajaca
-# następujące pola $data $fact $k $metric bedace odpowiednio
-# tablica danych, wektorem kalsyfikacji, wartoscia k i metryka.
-# data musi zawierac dane niezbene do klasyfikacji
-# (data MUSI zawierac miec IDENTYCZNA strukture co
-# dane uzyte przy nauce klasyfikatora)
+# Rozszerzenie funkcji generycznej predict.  Obiekt model musi byc lista
+# zawierajaca następujące pola $vectors $fact $k $metric bedace odpowiednio
+# niezerową ramką danych zliczającą słowa, wektorem kalsyfikacji, wartoscia k i
+# metryka.  data musi zawierac dane niezbene do klasyfikacji (data MUSI
+# zawierac miec IDENTYCZNA strukture co dane uzyte przy nauce klasyfikatora)
 #
 # RETURN wektor która dla kazdego wiersza danych zwraca 
 # wiersz etykietę kategorii.
@@ -47,18 +69,28 @@ train.knn <- function(data, fact, k=3, metric=metric.cos) {
 predict.ptmKnn <- function(model, data) {
   
   result <- c()
+  cdata <- data.compact(data)
+
+  apply.metric <- function (v1,v2,metric){
+    mock.v2 <- data.frame (id=v2$id, count=v2$count*0)
+    m.v1 <- match(v1$id, v2$id)
+    c.v1 <- rbind (v1[is.na(m.v1),], v1[!is.na(m.v1),], mock.v2[-m.v1[!is.na(m.v1)],])
+    mock.v1 <- data.frame (id=v1$id, count=v1$count*0)
+    m.v2 <- match (v2$id, v1$id)
+    c.v2 <- rbind (mock.v1[-m.v2[!is.na(m.v2)],], v2[is.na(m.v2),], v2[!is.na(m.v2),])
+
+    return (metric (c.v1, c.v2))
+  }
 
   # przeglądamy wszystkie testowane przypadki
   for (i in 1:nrow(data)){
-    distances <- sapply (model$data, model$metric, data[i,])
-    order <- rev(order(distances))
+    distances <- sapply (model$vectors, apply.metric, cdata[[i]], model$metric)
+    order <- order(distances)
     voters <- table(model$fact[order[1:model$k]])
     voteing <- as.data.frame(voters)
     voteing_order <- rev(order(voteing$Freq))
-    voteing_res <- voteing$voters[voteing_order[1]]
-    voteing_res <- 1 
-    
-    result <- append(result, voteing_res)
+    voteing_res <- voteing[voteing_order[1],1]
+    result <- append(result, levels(model$fact)[voteing_res])
   }
 	return (result)
 }
